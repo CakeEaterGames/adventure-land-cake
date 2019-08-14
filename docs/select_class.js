ae63a6fa89cd7e1e07b86f59a553e8bf1abb3c5a
Ruslan Taranov <tortoed5@outlook.com>
2019-08-14 14:54:54 +1100

    asd

--- docs/select_class.js
+++ docs/select_class.js
@@ -9,19 +9,19 @@ if(character.ctype == "merchant")
 else if(character.ctype == "warrior")
 {
   $.getScript("https://cakeeatergames.github.io/adventure-land-cake/warrior.js", function() {
-    game_log("merchant");
+    game_log("warrior");
   });
 }
 else if(character.ctype == "priest")
 {
   $.getScript("https://cakeeatergames.github.io/adventure-land-cake/priest.js", function() {
-    game_log("merchant");
+    game_log("priest");
   });
 }
 else if(character.ctype == "mage")
 {
   $.getScript("https://cakeeatergames.github.io/adventure-land-cake/mage.js", function() {
-    game_log("merchant");
+    game_log("mage");
   });
 }else {
     game_log("unknown class");
