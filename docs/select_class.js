1ea8ed4f2407d49d68978dbe376f00f209d2f0d8
Ruslan Taranov <tortoed5@outlook.com>
2019-08-14 22:48:07 +1100

    asdf

--- docs/select_class.js
+++ docs/select_class.js
@@ -1,28 +1,31 @@
 game_log("choosing class...");
 
 
-$.getScript("https://cakeeatergames.github.io/adventure-land-cake/common.js");
+$.getScript("https://cakeeatergames.github.io/adventure-land-cake/common.js",load_class);
 
+function load_class()
+{
 
+  if(character.ctype == "merchant")
+  {
+    $.getScript("https://cakeeatergames.github.io/adventure-land-cake/merch.js");
+  }
+  else if(character.ctype == "warrior")
+  {
+    $.getScript("https://cakeeatergames.github.io/adventure-land-cake/warrior.js");
+  }
+  else if(character.ctype == "priest")
+  {
+    $.getScript("https://cakeeatergames.github.io/adventure-land-cake/priest.js");
+  }
+  else if(character.ctype == "mage")
+  {
+    $.getScript("https://cakeeatergames.github.io/adventure-land-cake/mage.js");
+  }
+  else {
+    game_log("unknown class");
+  }
 
-if(character.ctype == "merchant")
-{
-  $.getScript("https://cakeeatergames.github.io/adventure-land-cake/merch.js");
-}
-else if(character.ctype == "warrior")
-{
-  $.getScript("https://cakeeatergames.github.io/adventure-land-cake/warrior.js");
-}
-else if(character.ctype == "priest")
-{
-  $.getScript("https://cakeeatergames.github.io/adventure-land-cake/priest.js");
-}
-else if(character.ctype == "mage")
-{
-  $.getScript("https://cakeeatergames.github.io/adventure-land-cake/mage.js");
-}
-else {
-  game_log("unknown class");
 }
 
 
