3970084f773345c0167f7acd32ea7afc336a04bc
Ruslan Taranov <tortoed5@outlook.com>
2019-08-16 15:19:15 +1100

    asd

--- docs/merch.js
+++ docs/merch.js
@@ -1,1 +1,17 @@
 game_log("merch");
+
+function update_state() {
+  common_update_state();
+  switch (state) {
+
+  }
+}
+function enter_state(s) {
+  common_enter_state(s);
+  switch (state) {
+  
+  }
+}
+function leave_state(s) {
+  common_leave_state(s);
+}
